db1bbd52e9d287ca955fbeed8de80926efde735c
Matt Reishman <matt.reishman@me.com>
2018-11-04 08:03:40 -0600

    Added mobile navigation

--- core/content/base/header.php
+++ core/content/base/header.php
@@ -1,8 +1,23 @@
 <header>
 	<nav class="navigationMain" >
-		<?php 
-		$navArr = $header->generateNavigationArray();
-		echo $header->generateNavUL($navArr);
-		?>
+		<input type="checkbox" id="mainNavigationToggle" checked>
+		<label for="mainNavigationToggle" class="mobileNavButton">
+			<span class="navIconChecked">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</span>
+			<span class="navIconNotChecked">
+				<span class="icon-bar-top"></span>
+				<span class="icon-bar-mid"></span>
+				<span class="icon-bar-bot"></span>
+			</span>
+		</label>
+		<span class="navigationSpan" >
+			<?php 
+			$navArr = $header->generateNavigationArray();
+			echo $header->generateNavUL($navArr);
+			?>
+		</span>
 	</nav>
 </header>
